83753e747eed912205a492358cde682932ec1dd2
Kamil Krzywicki <kamil.krzywicki@dbschenker.com>
2022-10-24 11:39:57 +0200

    bump scheduler config to v1beta2

--- pkg/thunderingherdscheduling/configregister.go
+++ pkg/thunderingherdscheduling/configregister.go
@@ -4,20 +4,20 @@ import (
 	"k8s.io/apimachinery/pkg/conversion"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	schedschemev1beta1 "k8s.io/kube-scheduler/config/v1beta1"
+	schedschemev1beta2 "k8s.io/kube-scheduler/config/v1beta2"
 )
 
 const GroupName = "kubescheduler.config.k8s.io"
 
-var SchemaVersionV1Beta1 = schema.GroupVersion{Group: GroupName, Version: "v1beta1"}
+var SchemaVersionV1Beta2 = schema.GroupVersion{Group: GroupName, Version: "v1beta2"}
 var SchemeGroupVersionInternal = schema.GroupVersion{Group: GroupName, Version: runtime.APIVersionInternal}
 
 var (
-	localSchemeBuilder = &schedschemev1beta1.SchemeBuilder
+	localSchemeBuilder = &schedschemev1beta2.SchemeBuilder
 )
 
 func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemaVersionV1Beta1, &ThunderingHerdSchedulingArgs{})
+	scheme.AddKnownTypes(SchemaVersionV1Beta2, &ThunderingHerdSchedulingArgs{})
 	scheme.AddKnownTypes(SchemeGroupVersionInternal, &ThunderingHerdSchedulingArgs{})
 	scheme.AddTypeDefaultingFunc(&ThunderingHerdSchedulingArgs{}, func(obj interface{}) {
 		SetDefaultThunderingHerdArgs(obj.(*ThunderingHerdSchedulingArgs))
