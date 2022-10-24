440e2692fb3714da60b2e930faae54ed63af157f
Kamil Krzywicki <kamil.krzywicki@dbschenker.com>
2022-10-24 12:41:49 +0200

    switch to kubescheduler.config.k8s.io/v1beta3 enable unauthoenticated metrics endpoint in helm chart

--- pkg/thunderingherdscheduling/configregister.go
+++ pkg/thunderingherdscheduling/configregister.go
@@ -4,20 +4,20 @@ import (
 	"k8s.io/apimachinery/pkg/conversion"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	schedschemev1beta2 "k8s.io/kube-scheduler/config/v1beta2"
+	schedschemev1beta3 "k8s.io/kube-scheduler/config/v1beta3"
 )
 
 const GroupName = "kubescheduler.config.k8s.io"
 
-var SchemaVersionV1Beta2 = schema.GroupVersion{Group: GroupName, Version: "v1beta2"}
+var SchemaVersionV1Beta3 = schema.GroupVersion{Group: GroupName, Version: "v1beta3"}
 var SchemeGroupVersionInternal = schema.GroupVersion{Group: GroupName, Version: runtime.APIVersionInternal}
 
 var (
-	localSchemeBuilder = &schedschemev1beta2.SchemeBuilder
+	localSchemeBuilder = &schedschemev1beta3.SchemeBuilder
 )
 
 func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemaVersionV1Beta2, &ThunderingHerdSchedulingArgs{})
+	scheme.AddKnownTypes(SchemaVersionV1Beta3, &ThunderingHerdSchedulingArgs{})
 	scheme.AddKnownTypes(SchemeGroupVersionInternal, &ThunderingHerdSchedulingArgs{})
 	scheme.AddTypeDefaultingFunc(&ThunderingHerdSchedulingArgs{}, func(obj interface{}) {
 		SetDefaultThunderingHerdArgs(obj.(*ThunderingHerdSchedulingArgs))
